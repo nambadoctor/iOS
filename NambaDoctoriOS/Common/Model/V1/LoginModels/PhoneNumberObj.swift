9e77100605322788be80af8be88198269bbf562b
suryam0801@gmail.com <58582997+suryam0801@users.noreply.github.com>
2021-02-15 16:37:26 -0800

    moved around some files

---
+++ NambaDoctoriOS/Common/Model/V1/LoginModels/PhoneNumberObj.swift
@@ -0,0 +1,13 @@
+//
+//  PhoneNumberEntry.swift
+//  NambaDoctoriOS
+//
+//  Created by Surya Manivannan on 25/01/21.
+//
+
+import SwiftUI
+
+struct PhoneNumberObj {
+    var countryCode:String = "+91"
+    var number:String = ""
+}
