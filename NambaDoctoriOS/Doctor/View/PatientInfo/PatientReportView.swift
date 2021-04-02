89b008a31cb6793ccdc087644ab781de85adcd83
Surya <Suryam0801@gmail.com>
2021-04-02 15:40:13 -0700

    minor bug fixes

--- NambaDoctoriOS/Doctor/View/PatientInfo/PatientReportView.swift
+++ NambaDoctoriOS/Doctor/View/PatientInfo/PatientReportView.swift
@@ -20,16 +20,13 @@ struct PatientReportView: View {
             case .loading:
                 Indicator()
             case .notFound:
-                Text("Report could not load")
-                    .foregroundColor(.yellow)
-                    .bold()
-                    .padding()
+                EmptyView()
             case .display:
                 ImageView(withURL: patientReportVM.mediaURL!)
             }
         }
-        .frame(width: 100, height: 160)
-        .background(Color.yellow.opacity(0.2))
+        .frame(width: patientReportVM.displayImage == .display ? 100 : 0,
+               height: patientReportVM.displayImage == .display ? 160 : 0)
         .cornerRadius(10)
         .padding([.top, .trailing])
     }
