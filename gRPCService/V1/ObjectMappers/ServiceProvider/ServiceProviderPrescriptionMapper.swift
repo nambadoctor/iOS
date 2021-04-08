f63a8989b9276eab3b797407e761d3eb36653d25
Surya <Suryam0801@gmail.com>
2021-04-08 16:22:29 -0700

    more minor fix

--- gRPCService/V1/ObjectMappers/ServiceProvider/ServiceProviderPrescriptionMapper.swift
+++ gRPCService/V1/ObjectMappers/ServiceProvider/ServiceProviderPrescriptionMapper.swift
@@ -20,7 +20,7 @@ class ServiceProviderPrescriptionMapper {
     
     func localPrescriptionToGrpc (prescription: ServiceProviderPrescription) -> Nd_V1_ServiceProviderPrescriptionMessage {
         return Nd_V1_ServiceProviderPrescriptionMessage.with {
-            $0.prescriptionID = prescription.prescriptionID.toProto
+            $0.prescriptionID = ""//prescription.prescriptionID.toProto
             $0.serviceRequestID = prescription.serviceRequestID.toProto
             $0.customerID = prescription.customerID.toProto
             $0.createdDateTime = prescription.createdDateTime.toProto
