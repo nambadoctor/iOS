3464b34ce6ecb20fc12d5b96e09bd2ea20afd0a3
Surya <Suryam0801@gmail.com>
2021-04-21 01:03:54 -0700

    stable with notification navigation

--- NambaDoctoriOS/Doctor/ViewModel/IntermediateAppointmentViewModel.swift
+++ NambaDoctoriOS/Doctor/ViewModel/IntermediateAppointmentViewModel.swift
@@ -80,7 +80,6 @@ class IntermediateAppointmentViewModel : ObservableObject {
         checkIfAppointmentFinished()
         checkIfAppointmentStarted()
         getClinicalInfoCollapseSetting()
-        checkForDirectNavigation()
     }
 }
 
