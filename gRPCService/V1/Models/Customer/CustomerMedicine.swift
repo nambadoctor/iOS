2ecf3c271523411280519e6ca4f13b6d9e8fccf4
Surya <Suryam0801@gmail.com>
2021-05-08 02:29:56 -0700

    un-committed files committed

---
+++ gRPCService/V1/Models/Customer/CustomerMedicine.swift
@@ -0,0 +1,19 @@
+//
+//  CustomerMedicineMessage.swift
+//  NambaDoctoriOS
+//
+//  Created by Surya Manivannan on 4/26/21.
+//
+
+import Foundation
+
+struct CustomerMedicine {
+    var medicineName:String
+    var dosage:String
+    var routeOfAdministration:String
+    var intake:String
+    var duration:Int32
+    var timings:String
+    var specialInstructions:String
+    var medicineID:String
+}
