d062f5ba82cdc6f4667c5d61b31a23d9e6c5b39b
Surya <Suryam0801@gmail.com>
2021-05-09 17:42:25 -0700

    customer profile added

--- NambaDoctoriOS/Customer/View/CustomerProfile/CustomerProfileView.swift
+++ NambaDoctoriOS/Customer/View/CustomerProfile/CustomerProfileView.swift
@@ -12,44 +12,46 @@ struct CustomerProfileView: View {
     @EnvironmentObject var customerVM:CustomerViewModel
     
     var body: some View {
-        VStack (alignment: .leading) {
-            VStack {
-                ImageView(imageLoader: customerVM.imageLoader!)
-                
-                VStack (alignment: .leading, spacing: 10) {
-                    VStack (alignment: .leading, spacing: 3) {
-                        Text("Name:")
-                            .font(.footnote)
-                            .bold()
-                            .foregroundColor(.gray)
-                        Text("\(customerVM.customerProfile!.firstName) \(customerVM.customerProfile!.lastName)")
-                    }
-                    
-                    VStack (alignment: .leading, spacing: 3) {
-                        Text("Age:")
-                            .font(.footnote)
-                            .bold()
-                            .foregroundColor(.gray)
-                        Text("\(customerVM.customerProfile!.age)")
-                    }
-                    
-                    VStack (alignment: .leading, spacing: 3) {
-                        Text("Gender:")
-                            .font(.footnote)
-                            .bold()
-                            .foregroundColor(.gray)
-                        Text("\(customerVM.customerProfile!.gender)")
-                    }
-                    
-                    VStack (alignment: .leading, spacing: 3) {
-                        Text("Phone Number:")
-                            .font(.footnote)
-                            .bold()
-                            .foregroundColor(.gray)
-                        Text("\(customerVM.customerProfile!.phoneNumbers[0].countryCode)\(customerVM.customerProfile!.phoneNumbers[0].number)")
-                    }
-                }.padding()
+        VStack (alignment: .center) {
+
+            ImageView(imageLoader: customerVM.imageLoader!)
+            
+            VStack (alignment: .leading, spacing: 10) {
+                VStack (alignment: .leading, spacing: 3) {
+                    Text("Name:")
+                        .font(.footnote)
+                        .bold()
+                        .foregroundColor(.gray)
+                    Text("\(customerVM.customerProfile!.firstName) \(customerVM.customerProfile!.lastName)")
+                }
+
+                VStack (alignment: .leading, spacing: 3) {
+                    Text("Age:")
+                        .font(.footnote)
+                        .bold()
+                        .foregroundColor(.gray)
+                    Text("\(customerVM.customerProfile!.age)")
+                }
+
+                VStack (alignment: .leading, spacing: 3) {
+                    Text("Gender:")
+                        .font(.footnote)
+                        .bold()
+                        .foregroundColor(.gray)
+                    Text("\(customerVM.customerProfile!.gender)")
+                }
+
+                VStack (alignment: .leading, spacing: 3) {
+                    Text("Phone Number:")
+                        .font(.footnote)
+                        .bold()
+                        .foregroundColor(.gray)
+                    Text("\(customerVM.customerProfile!.phoneNumbers[0].countryCode)\(customerVM.customerProfile!.phoneNumbers[0].number)")
+                }
+                HStack {Spacer()}
             }
-        }
+
+            Spacer()
+        }.padding()
     }
 }
