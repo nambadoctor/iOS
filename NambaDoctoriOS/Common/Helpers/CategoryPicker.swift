63b4b316e4d9ea0ab11a935e4910a865c8420f70
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2021-05-26 17:00:29 -0700

    added categories picker

---
+++ NambaDoctoriOS/Common/Helpers/CategoryPicker.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryPicker.swift
+//  NambaDoctoriOS
+//
+//  Created by Surya Manivannan on 5/25/21.
+//
+
+import SwiftUI
+
+struct CategoryPicker : View {
+    @Binding var categoriesList:[String]
+    @Binding var selectedCategory:String
+    
+    var body: some View {
+        HStack {
+            ScrollView (.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach (self.categoriesList, id: \.self) {category in
+                        Text(category)
+                            .padding(10)
+                            .foregroundColor(category == self.selectedCategory ? Color.white : Color.black)
+                            .background(category == self.selectedCategory ? Color.blue : Color.gray.opacity(0.2))
+                            .cornerRadius(10)
+                            .onTapGesture {
+                                self.selectedCategory = category
+                            }
+                    }
+                }
+            }
+        }
+        .padding(.leading)
+    }
+}
