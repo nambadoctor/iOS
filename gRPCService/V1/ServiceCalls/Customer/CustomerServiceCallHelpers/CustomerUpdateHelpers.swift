913b53977c3da257f52975b742600ecf2435eda6
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2021-06-16 14:32:09 -0700

    nullable customer profile values

--- gRPCService/V1/ServiceCalls/Customer/CustomerServiceCallHelpers/CustomerUpdateHelpers.swift
+++ gRPCService/V1/ServiceCalls/Customer/CustomerServiceCallHelpers/CustomerUpdateHelpers.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class CustomerUpdateHelpers {
     func UpdateFCMToken (customerId:String, completion: @escaping (_ response:String?)->()) {
-        let customer = CustomerProfile(customerID: customerId, appInfo: CustomerAppInfo(authID: "", authType: "", deviceToken: DeviceTokenId, appInfoID: "", deviceTokenType: "apn"))
+        let customer = CustomerProfile(customerID: customerId, appInfo: CustomerAppInfo(authID: AuthenticateService().getUserId(), authType: "Firebase", deviceToken: DeviceTokenId, appInfoID: "", deviceTokenType: "apn"))
         
         CustomerProfileService().setCustomerProfile(customerProfile: customer) { response in
             completion(response)
