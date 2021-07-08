6503e7e3c4e02fb1312c93411c9b34cc59fc82b1
suryam0801 <58582997+suryam0801@users.noreply.github.com>
2021-07-07 21:27:43 -0700

    checkin

--- gRPCService/V1/ServiceCalls/Common/LoggerService.swift
+++ gRPCService/V1/ServiceCalls/Common/LoggerService.swift
@@ -11,7 +11,7 @@ var SessionId:String = ""
 var CorrelationId:String = ""
 var AppointmentID:String = ""
 var EventMessage:String = ""
-var VersionNumber:String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.36"
+var VersionNumber:String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.37"
 
 class LoggerService {
     func log(eventName:String) {
